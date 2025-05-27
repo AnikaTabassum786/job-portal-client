02d046570d0a3ea21c33a86450803266bf67a50c
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-27 10:58:25 +0600

    Apply Framer Motion

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
             This is Home page
+            <div>
+                <Banner></Banner>
+            </div>
         </div>
     );
 };
