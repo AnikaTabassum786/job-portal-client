69f458264e463974ae19bb80f99f36f412f0f7b6
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-27 12:41:44 +0600

    Apply motion in banner

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -5,7 +5,7 @@ import Navbar from '../shared/Navbar';
 
 const RootLayout = () => {
     return (
-        <div>
+        <div className='container mx-auto'>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import { color, motion } from "motion/react"
+import team1 from '../../assets/images/team-1.jpg'
+import team2 from '../../assets/images/team-2.jpg'
 
 const Banner = () => {
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img
+                <div className='flex-1'>
+                    {/* <img
                     src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
                     className="max-w-sm rounded-lg shadow-2xl"
-                />
-                <div>
-                    <motion.h1
+                /> */}
+                    <motion.img 
+                    animate={{y:[100,150,100]}}
+                    transition={{duration:5, repeat:Infinity}}
+                    className='max-w-sm rounded-t-[40px] rounded-br-[40px] shadow-2xl
+                    border-s-8 border-b-8 border-s-blue-500 border-b-blue-500
+                    ' 
+                    src={team1} alt="" />
+                    <motion.img 
+                    animate={{x:[100,150,100]}}
+                    transition={{duration:10, delay:2, repeat:Infinity}}
+                    className='max-w-sm rounded-t-[40px] rounded-br-[40px] shadow-2xl
+                    border-s-8 border-b-8 border-s-blue-500 border-b-blue-500
+                    ' 
+                    src={team2} alt="" />
+                </div>
+                <div className='flex-1'>
+                    {/* <motion.h1
                         animate={{ rotate: 180 }}
                         transition={{ duration: 2 }}
-                        className="text-5xl font-bold">Latest Job Offer</motion.h1>
+                        className="text-5xl font-bold">Latest Job Offer</motion.h1> */}
 
                     <motion.h1
                         initial={{ scale: 0 }}
@@ -22,9 +40,10 @@ const Banner = () => {
                         className="text-5xl font-bold">Remote
                         <motion.span
                             animate={
-                                { color: ['#ff5733', '#33ff33', '#8a33ff'],
-                                  transition:{duration:4,repeat:Infinity}
-                                 }}
+                                {
+                                    color: ['#ff5733', '#33ff33', '#8a33ff'],
+                                    transition: { duration: 4, repeat: Infinity }
+                                }}
                         >Job</motion.span> Offer</motion.h1>
 
 
