c38505afec950baa16a33bbe227331978e5cb29d
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-29 12:20:06 +0600

    Done

--- src/pages/MyApplication/MyApplication.jsx
+++ src/pages/MyApplication/MyApplication.jsx
@@ -1,12 +1,19 @@
 import React, { Suspense } from 'react';
 import ApplicationStat from './ApplicationStat';
 
+import ApplicationList from './ApplicationList.jsx';
+import { myApplicationPromise } from '../../api/applicationAPI.js';
+import useAuth from '../../hooks/useAuth.jsx';
+
 const MyApplication = () => {
+
+     const {user} = useAuth()
+
     return (
         <div>
             <ApplicationStat></ApplicationStat>
             <Suspense fallback={'loading applications'}>
-                <MyApplication></MyApplication>
+               <ApplicationList myApplicationPromise={myApplicationPromise(user.email)}></ApplicationList>
             </Suspense>
         </div>
     );
