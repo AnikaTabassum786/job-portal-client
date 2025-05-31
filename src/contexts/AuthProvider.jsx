3a6798695e8eda734cc327429386ba7ccaa4d864
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-31 12:38:49 +0600

    Start JWT

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { auth } from '../firebase/firebase.init';
 import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import axios from 'axios';
 
 const provider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
-    const [user,setUser] = useState(null)
+    const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
 
     const createUser = (email, password) => {
@@ -14,31 +15,43 @@ const AuthProvider = ({ children }) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const signInUser=( email, password)=>{
+    const signInUser = (email, password) => {
         setLoading(true)
-        return signInWithEmailAndPassword(auth,email,password)
+        return signInWithEmailAndPassword(auth, email, password)
     }
-    const signInWithGoogleLogin=()=>{
+    const signInWithGoogleLogin = () => {
         setLoading(true)
         return signInWithPopup(auth, provider)
     }
 
-    const signOutUser=()=>{
+    const signOutUser = () => {
         setLoading(true)
         return signOut(auth)
     }
 
-    useEffect(()=>{
-        const unSubscribe = onAuthStateChanged(auth,currentUser =>{
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            setLoading(false)
-            console.log('user in the auth state change',currentUser)
+            setLoading(false);
+            if (currentUser?.email) {
+                const userData = { email: currentUser.email }
+                axios.post('', userData)
+                    .then(res => {
+                        console.log('Token After jwt', res.data)
+                        // localStorage
+                        const token = res.data.token;
+                        localStorage.setItem('token',token)
+                    })
+                    .cath(error => console.log(error))
+            }
+
+            console.log('user in the auth state change', currentUser)
         })
 
         return () => {
             unSubscribe();
         }
-    },[])
+    }, [])
 
     const authInfo = {
         user,
